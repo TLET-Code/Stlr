29dff52808e9ad931b0de6143ea194b8d94b3b02
ItsTato <78940659+ItsTato@users.noreply.github.com>
2025-08-19 11:30:28 -0600

    woah what u can pull the version? like in setup.py? thats so revolutionary11111

--- stlr/__init__.py
+++ stlr/__init__.py
@@ -1,2 +1,4 @@
 from .app import Stlr
 from .Enum import HTTP
+
+__version__:str = "1.0.0a"
--- stlr/cli.py
+++ stlr/cli.py
@@ -1,1 +1,17 @@
-# to build
+import click
+from stlr import __version__
+
+@click.group()
+def cli():
+	"""Stlr - A fast, modular & simple Python web framework."""
+	pass
+
+@cli.command()
+def run():
+	"""Serve a Stlr app in development mode forever."""
+	click.echo("...")
+
+@cli.command()
+def version():
+	"""Prints the current version and exits."""
+	click.echo(__version__)
